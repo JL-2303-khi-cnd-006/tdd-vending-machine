f2b98610743ebdb1346d113a0eef0e910bd9b7e4
Javeria Lodhi <javeria.lodhi@xloopdigital.com>
2023-04-13 09:37:32 +0500

    First Test Failed

--- src/Machine.js
+++ src/Machine.js
@@ -1,9 +1,41 @@
 module.exports = class Machine {
     constructor() {
+    this.items = [
+    { itemName:'crisps', itemPrice: 100},
+    { itemName:'chocolate', itemPrice: 100 },
+    { itemName:'mint', itemPrice: 70 },
+ ];
 
+// this.items = [
+//     { itemName:'crisps', itemPrice: 100},
+//     {itemName:'chocolate', itemPrice: 100 },
+//     {  itemName:'mint', itemPrice: 70 },
+//  ];
+//     this.amountDeposit = 0;
+//     this.validAmount = [10,20,50,100,500,1000,5000];
+// }
+    // seeSelections() {
+    //     return this.items;
+    // }
     }
+}
+//     deposit(amount)
+//     {
+//         if(this.validAmount.includes(amount)) {
+//             this.amountDeposit += amount;
+//             let res = `you have deposited Rs ${this.amountDeposit}`;  
+//             return res;      
+//         }
+//         else {
+//             return "Invalid amount entered";
+//         }
+//     }
 
-
-    seeSelections() {
-    }
-};
+//    selectItem(code)
+// {
+//    let invalidCode = 'Vend56';
+//    if(code == invalidCode){
+//     return `your item with ${code} not found`;
+//    }
+// }
+//     }
