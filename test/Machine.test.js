6e92d3e2b15750226abe63cf2c99406fbc7cab16
Javeria Lodhi <javeria.lodhi@xloopdigital.com>
2023-04-13 09:39:55 +0500

    First Test Failed

--- test/Machine.test.js
+++ test/Machine.test.js
@@ -5,8 +5,10 @@ describe('the vending machine', () => {
         // setup
         const machine = new Machine();
         const expected = [{itemName: 'crisps', itemPrice: 100}, {itemName: 'chocolate',itemPrice: 100}, {itemName:'mint', itemPrice: 70}];
+        
         // exercise
         const actual = machine.seeSelections();
+        
         // assert
         expect(actual).toEqual(expected); 
     });
